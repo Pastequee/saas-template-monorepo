ccd61627d680904a3f7e747686ebe5e55ad96690
Pasteque <contact@pastek.xyz>
2025-11-30 17:25:10 +0100

    chore: update better-auth config to include usefull plugins

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,8 +1,9 @@
+import { admin, lastLoginMethod } from 'better-auth/plugins'
 import { createAuthClient } from 'better-auth/react'
 import { env } from './env'
 
 export const authClient = createAuthClient({
-	// plugins: [tanstackStartCookies()],
+	plugins: [lastLoginMethod(), admin()],
 	baseURL: env.VITE_BACKEND_URL,
 })
 
--- packages/auth/src/auth-config.ts
+++ packages/auth/src/auth-config.ts
@@ -1,24 +1,36 @@
 import { db } from '@repo/db-prisma'
 import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
-import { openAPI } from 'better-auth/plugins'
+import { admin, lastLoginMethod, openAPI } from 'better-auth/plugins'
 import { randomUUIDv7 } from 'bun'
 import { env } from './env'
 
 export const auth = betterAuth({
-	secret: env.BETTER_AUTH_SECRET,
 	database: prismaAdapter(db, { provider: 'postgresql' }),
-	emailAndPassword: { enabled: true },
+
+	secret: env.BETTER_AUTH_SECRET,
 	trustedOrigins: [env.FRONTEND_URL],
+
+	experimental: {
+		joins: true,
+	},
+
 	advanced: {
 		database: {
 			generateId: () => randomUUIDv7(),
 		},
 	},
-	experimental: {
-		joins: true,
+
+	emailAndPassword: { enabled: true },
+
+	socialProviders: {
+		google: {
+			clientId: env.GOOGLE_CLIENT_ID,
+			clientSecret: env.GOOGLE_CLIENT_SECRET,
+		},
 	},
-	plugins: [openAPI()],
+
+	plugins: [openAPI(), admin(), lastLoginMethod()],
 })
 
 export type Auth = typeof auth
