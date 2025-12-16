b178d4ef39e87e2d9287d97ddc47bfe588e68b37
Pasteque <contact@pastek.xyz>
2025-12-16 20:03:45 +0100

    fix: drizzle beta not compatible with better-auth relation joins

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -3,4 +3,4 @@ import { env } from './env'
 import { relations } from './relations'
 import * as schema from './schemas'
 
-export const db = drizzle(env.DATABASE_URL, { relations, schema })
+export const db = drizzle(env.DATABASE_URL, { relations, schema, casing: 'snake_case' })
